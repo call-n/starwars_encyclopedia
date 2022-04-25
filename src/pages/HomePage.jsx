72a4628dfb550b59b0b453d55191fbb1509f23a6
call-n <callenilsson2001@gmail.com>
2022-04-25 12:47:03 +0200

    Homepage finished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
-    <h1>lol</h1>  
+    <>
+        <h1>Welcome to the Star Wars Encyclopedia</h1>
+
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <div style={{marginBottom: "5px"}}>
+                <Button variant="primary" as={Link} to="/films">Have a look at all the films</Button>
+            </div>
+            <div>
+                <Button variant="info" as={Link} to="/people">Or take a look at all the people in the films</Button>
+            </div>
+        </div>
+    </>
   )
 }
 
